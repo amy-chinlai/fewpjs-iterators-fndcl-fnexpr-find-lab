e4d5ca1f5fe56190aed1f3e17a6b27e7d44dc401
amy-chinlai <amy@actionnetwork.org>
2021-12-20 13:43:27 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
+function superbowlWin(records) {
+    let winner = records.find(function(r) { return r.result === "W" })
+    console.log(winner)
+    return !!winner ? winner.year : undefined
+}
 
+// playing
+
+const records = record.map(record => console.log(record.year))
